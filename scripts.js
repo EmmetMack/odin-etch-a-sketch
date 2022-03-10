629416a8f4de2bd55ba60e28c9de828bbf2edf56
EmmetMack <emmet.mack10@gmail.com>
2022-03-10 14:43:55 -0600

    able to hover and add color to divs

--- scripts.js
+++ scripts.js
@@ -1,6 +1,5 @@
 //create 16x16 grid with JS
 function createGrid() {
-    console.log('createGrid');
     for(let i = 0; i < 16; i ++) {
         for(let j = 0; j < 16; j++) {
             //loop through sixteen rows and sixteen cols per row
@@ -8,11 +7,15 @@ function createGrid() {
             //figure out how to display them
             let newDiv = document.createElement('div');
             newDiv.setAttribute('class', 'grid-item');
+            newDiv.addEventListener('mouseover', addColor);
             const container = document.querySelector(".container");
-            console.log('container', container);
             container.appendChild(newDiv);
-
         }
     }
 }
+
 window.addEventListener('load', createGrid);
+
+function addColor() {
+    this.classList.add('hovered');
+}
